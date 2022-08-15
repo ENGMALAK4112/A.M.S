40707329f54b79ae32c7bd4d916eaff3d9bbb880
ENG: Malak <110707939+ENGMALAK4112@users.noreply.github.com>
2022-08-15 21:08:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace كود_حساب_العمر
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime MyAge;
+            Console.Write("Write Your Birth Day: ");
+            MyAge = Convert.ToDateTime(Console.ReadLine());
+            int year = Convert.ToInt32(DateTime.Now.Subtract(MyAge).TotalDays) / 360;
+            Console.WriteLine("your Age is: " + year + "Years");
+            Console.ReadKey();
+           // Activity workflow1 = new Workflow1();
+            //WorkflowInvoker.Invoke(workflow1);
+        }
+    }
+}
